84fc14c9427089d9ffd409ad2bb676bd659deac3
kuum97 <113277713+kuum97@users.noreply.github.com>
2023-06-09 12:05:57 +0000

    Add timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,27 @@
+import { useEffect, useState } from "react";
+
+
 export default function App() {
-  return <div />;
+  const [timer, setTimer] = useState<number>(25 * 60);
+  const [isRunning, setIsRunning] = useState<boolean>(false);
+  const toggleTimer = () => {
+    setInterval(() => {
+      setTimer(prevTimer => prevTimer-1)
+    }, 1000)
+    setIsRunning(prevState => !prevState)
+  }
+  const formatTime = (time: number): string => {
+    const minutes = Math.floor(time / 60).toString().padStart(2, '0');
+    const seconds = (time % 60).toString().padStart(2, '0');
+    return `${minutes}:${seconds}`;
+  };
+  return <div>
+    <h1>Pomodoro</h1>
+    <div>
+      <p>{formatTime(timer)}</p>
+      <button onClick={toggleTimer}>{isRunning ? "pause" : "start"}</button>
+    </div>
+    <span>0/4 ROUND</span>
+    <span>0/12 GOAL</span>
+  </div>;
 }
